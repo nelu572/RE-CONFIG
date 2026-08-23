666e318dc0a0bcde365f291fc6f0c8791aebf474
nelu572 <s25031@gsm.hs.kr>
2026-08-23 17:04:17 +0900

    feat: ROOM 04 맵 추가

--- src/game/world.cpp
+++ src/game/world.cpp
@@ -6,6 +6,10 @@ static constexpr float T(int tiles) {
     return (float)(tiles * TILE_SIZE);
 }
 
+static constexpr float T(float tiles) {
+    return tiles * (float)TILE_SIZE;
+}
+
 static const RectF g_room00_platforms[] = {
     { T(6),  T(21), T(36), T(6) },
 };
@@ -45,6 +49,13 @@ static const SpeakerDevice g_room03_speakers[] = {
     { T(50), T(5), T(5), T(9) },
 };
 
+static const RectF g_room04_platforms[] = {
+    { T(0),  T(0),     T(48), T(4) },
+    { T(0),  T(21),    T(48), T(6) },
+    { T(0),  T(4),     T(3),  T(17) },
+    { T(45), T(4),     T(3),  T(17) },
+    { T(14), T(4),     T(20), T(16.25f) },
+};
 static const DeleteState g_default_delete_state = {};
 
 static const RoomDef g_rooms[] = {
@@ -52,6 +63,7 @@ static const RoomDef g_rooms[] = {
     { g_room01_platforms, (int)(sizeof(g_room01_platforms) / sizeof(g_room01_platforms[0])), g_room01_type_a_walls, (int)(sizeof(g_room01_type_a_walls) / sizeof(g_room01_type_a_walls[0])), 0, 0, { T(38), T(19), T(2), T(2) }, T(9), T(21) - 40.0f, { T(0), T(0), T(48), T(27) }, T(4), GRAVITY_DOWN, g_default_delete_state },
     { g_room02_platforms, (int)(sizeof(g_room02_platforms) / sizeof(g_room02_platforms[0])), g_room02_type_a_walls, (int)(sizeof(g_room02_type_a_walls) / sizeof(g_room02_type_a_walls[0])), 0, 0, { T(55), T(5), T(2), T(2) }, T(7), T(25) - 40.0f, { T(0), T(0), T(58), T(27) }, T(4), GRAVITY_DOWN, g_default_delete_state },
     { g_room03_platforms, (int)(sizeof(g_room03_platforms) / sizeof(g_room03_platforms[0])), 0, 0, g_room03_speakers, (int)(sizeof(g_room03_speakers) / sizeof(g_room03_speakers[0])), { T(54), T(21), T(2), T(2) }, T(6), T(23) - 40.0f, { T(0), T(0), T(60), T(27) }, T(4), GRAVITY_DOWN, g_default_delete_state },
+    { g_room04_platforms, (int)(sizeof(g_room04_platforms) / sizeof(g_room04_platforms[0])), 0, 0, 0, 0, { T(40), T(19), T(2), T(2) }, T(6), T(21) - 40.0f, { T(0), T(0), T(48), T(27) }, T(4), GRAVITY_DOWN, g_default_delete_state },
 };
 
 const RoomDef* GetRoom(int index) {
